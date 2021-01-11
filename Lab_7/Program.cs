2572eb12d1d60b7e3d93928e8c89b9436caf96ac
hyperstone1 <kingofthegorgon@mail.ru>
2021-01-11 04:19:04 +0300

    feat: add function AddApartment, DeleteApartment and UpdateApartment

--- Lab_7/Program.cs
+++ Lab_7/Program.cs
@@ -59,7 +59,80 @@ namespace Lab_7
             }
             Console.ReadLine();
         }
-     
+        public static void AddApartment()
+        {
+            Console.Write("Название id: ");
+            int id = System.Int32.Parse(Console.ReadLine());
+            Console.Write("Квартира: ");
+            string name = Console.ReadLine();
+            Console.Write("Кол-во комнат: ");
+            string quantity = Console.ReadLine();
+            Console.Write("Стоимость квартиры: ");
+            long price = System.Int64.Parse(Console.ReadLine());
+            Apartment apartment = new Apartment(id, name, quantity, price);
+
+            XDocument xdoc = XDocument.Load("apartment.xml");
+            XElement root = xdoc.Element("apartment");
+            root.Add(new XElement("apartment",
+                new XAttribute("id", apartment.GetId()),
+                new XElement("name", apartment.GetName()),
+                new XElement("quantity", apartment.GetQuantity()),
+                new XElement("price", apartment.GetPrice()))
+                );
+            xdoc.Save("apartment.xml");
+        }
+
+        public static void DeleteApartment()
+        {
+            Console.Clear();
+            Console.WriteLine("Айди квартиры:");
+            XDocument xdoc = XDocument.Load("apartment.xml");
+            XElement root = xdoc.Element("apartment");
+            int id = System.Int32.Parse(Console.ReadLine());
+            foreach (XElement xe in root.Elements("apartment").ToList())
+            {
+
+
+                if (xe.Attribute("id").Value == id.ToString())
+
+                {
+                    xe.Remove();
+                }
+            }
+            Console.Write("Квартира удалена из базы");
+            xdoc.Save("apartment.xml");
+        }
+
+
+        public static void UpdateApartment()
+        {
+            Console.Clear();
+            Console.WriteLine("Айди квартиры:");
+
+            XDocument xdoc = XDocument.Load("apartment.xml");
+            XElement root = xdoc.Element("apartment");
+            int id = System.Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Название квартиры:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Цена:");
+            long price = System.Int64.Parse(Console.ReadLine());
+            Console.WriteLine("Кол-во комнат:");
+            String quantity = Console.ReadLine();
+
+            foreach (XElement xe in root.Elements("apartment").ToList())
+            {
+                // изменяем название и цену
+                if (xe.Attribute("id").Value == id.ToString())
+                {
+                    xe.Element("name").Value = name;
+                    xe.Element("price").Value = price.ToString();
+                    xe.Element("quantity").Value = quantity;
+                }
+
+            }
+            xdoc.Save("apartment.xml");
+        }
+
         static void Main(string[] args)
         {
 
@@ -84,17 +157,17 @@ namespace Lab_7
                         }
                     case 2:
                         {
-                        //   AddApartment();
+                           AddApartment();
                             break;
                         }
                     case 3:
                         {
-                         //   DeleteApartment();
+                            DeleteApartment();
                             break;
                         }
                     case 4:
                         {
-                          //  UpdateApartment();
+                            UpdateApartment();
                             break;
                         }
                 }
