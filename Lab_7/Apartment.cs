8bbe9dc91f9c7ca66df4909391ecffefa45eb041
Maksim Sugak <thewilder123@gmail.com>
2021-01-11 03:32:03 +0300

    init: add Program.cs and Apartment.cs

---
+++ Lab_7/Apartment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_7
+{
+    public partial class Apartment
+    {
+        public Apartment(int id, string name, string quantity, long price)
+        {
+            this.id = id;
+            this.name = name;
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;   
+        }
+        public void SetQuantity(string quantity)
+        {
+            this.quantity = quantity;
+        }
+        public void SetPrice(long price)
+        {
+            this.price = price;
+        }
+    }
+}
---
+++ Lab_7/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Xml.Linq;
+using System.IO;
+using System.Linq;
+
+namespace Lab_7
+{
+
+    public partial class Apartment
+    {
+        private int id;
+        private String name;
+        private String quantity;
+        private long price;
+        
+
+
+        public int GetId()
+        {
+            return id;
+        }
+        
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetQuantity()
+        {
+            return quantity;
+        }
+        public long GetPrice()
+        {
+            return price;
+        }
+    }
+
+    class Program
+    {
+
+        public static void GetInfo()
+        {
+            Console.Clear();
+            XDocument xdoc = XDocument.Load("apartment.xml");
+            foreach (XElement apartmentElement in xdoc.Element("apartment").Elements("apartment"))
+            {
+                XAttribute idElement = apartmentElement.Attribute("id");
+                XElement nameAttribute = apartmentElement.Element("name");
+                XElement quantityElement = apartmentElement.Element("quantity");
+                XElement priceElement = apartmentElement.Element("price");
+
+                if (idElement != null && nameAttribute != null && quantityElement != null && priceElement != null)
+                {
+                    Console.WriteLine($"Id: {idElement.Value}");
+                    Console.WriteLine($"Квартира: {nameAttribute.Value}");
+                    Console.WriteLine($"Кол-во комнат: {quantityElement.Value}");
+                    Console.WriteLine($"Цена: {priceElement.Value}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+     
+        static void Main(string[] args)
+        {
+
+            int choise = 0;
+            while (choise != 5)
+            {
+
+                Console.Clear();
+                Console.WriteLine("1)Просмотреть файл");
+                Console.WriteLine("2)Добавить");
+                Console.WriteLine("3)Удаление");
+                Console.WriteLine("4)Изменение");
+                Console.WriteLine("5)Выход");
+                choise = System.Int32.Parse(Console.ReadLine());
+
+                switch (choise)
+                {
+                    case 1:
+                        {
+                            GetInfo();
+                            break;
+                        }
+                    case 2:
+                        {
+                        //   AddApartment();
+                            break;
+                        }
+                    case 3:
+                        {
+                         //   DeleteApartment();
+                            break;
+                        }
+                    case 4:
+                        {
+                          //  UpdateApartment();
+                            break;
+                        }
+                }
+
+            }            
+        }
+    }
+}
